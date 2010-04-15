d32161a287aef8eff48e1ec11e0c8ca7117be5e5
Fernando Michelotti <fernando.michelotti@gmail.com>
2010-04-15 16:57:30 -0300

    adicionados: - Conversão de áudio - Extração de áudio

--- install.sh
+++ install.sh
@@ -2,8 +2,16 @@
 
 mkdir -p ~/.gnome2/nautilus-scripts/
 rm ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_video* rm ~/.gnome2/nautilus-scripts/PORTAL_Testar_video* 
+
 wget -O ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_video http://github.com/artistafrustrado/diaadiahelpers/raw/master/PORTAL_conversao_de_video
 chmod a+x ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_video
+
 wget -O ~/.gnome2/nautilus-scripts/PORTAL_Testar_video http://github.com/artistafrustrado/diaadiahelpers/raw/master/PORTAL_Testar_video
 chmod a+x ~/.gnome2/nautilus-scripts/PORTAL_Testar_video
+
+wget -O ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_audio http://github.com/artistafrustrado/diaadiahelpers/raw/master/PORTAL_conversao_de_audio
+chmod a+x ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_audio
+
+wget -O ~/.gnome2/nautilus-scripts/PORTAL_extracao_de_audio http://github.com/artistafrustrado/diaadiahelpers/raw/master/PORTAL_extracao_de_audio
+chmod a+x ~/.gnome2/nautilus-scripts/PORTAL_extracao_de_audio
 
