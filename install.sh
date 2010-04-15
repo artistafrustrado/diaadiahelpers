ad6b40d91c83a6346ef39cb064713243606b3ebd
Fernando Michelotti <fernando.michelotti@gmail.com>
2010-04-15 17:01:37 -0300

    Atualizado o install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/.gnome2/nautilus-scripts/
-rm ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_video* rm ~/.gnome2/nautilus-scripts/PORTAL_Testar_video* 
+rm ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_video* ~/.gnome2/nautilus-scripts/PORTAL_Testar_video* ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_audio* ~/.gnome2/nautilus-scripts/PORTAL_extracao_de_audio 
 
 wget -O ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_video http://github.com/artistafrustrado/diaadiahelpers/raw/master/PORTAL_conversao_de_video
 chmod a+x ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_video
