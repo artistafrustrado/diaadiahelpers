f633d6b85e688909dbf4c4ed971d0ba78e7e72fd
Fernando Michelotti <artista@frustrado.com.br>
2010-11-09 12:32:52 -0200

    modified wget parameters so it will not check for valid ssl certificates

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 mkdir -p ~/.gnome2/nautilus-scripts/
 rm ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_video* ~/.gnome2/nautilus-scripts/PORTAL_Testar_video* ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_audio* ~/.gnome2/nautilus-scripts/PORTAL_extracao_de_audio 
 
-wget -O ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_video http://github.com/artistafrustrado/diaadiahelpers/raw/master/PORTAL_conversao_de_video
+wget --no-check-certificate -O ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_video http://github.com/artistafrustrado/diaadiahelpers/raw/master/PORTAL_conversao_de_video
 chmod a+x ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_video
 
-wget -O ~/.gnome2/nautilus-scripts/PORTAL_Testar_video http://github.com/artistafrustrado/diaadiahelpers/raw/master/PORTAL_Testar_video
+wget --no-check-certificate -O ~/.gnome2/nautilus-scripts/PORTAL_Testar_video http://github.com/artistafrustrado/diaadiahelpers/raw/master/PORTAL_Testar_video
 chmod a+x ~/.gnome2/nautilus-scripts/PORTAL_Testar_video
 
-wget -O ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_audio http://github.com/artistafrustrado/diaadiahelpers/raw/master/PORTAL_conversao_de_audio
+wget --no-check-certificate -O ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_audio http://github.com/artistafrustrado/diaadiahelpers/raw/master/PORTAL_conversao_de_audio
 chmod a+x ~/.gnome2/nautilus-scripts/PORTAL_conversao_de_audio
 
-wget -O ~/.gnome2/nautilus-scripts/PORTAL_extracao_de_audio http://github.com/artistafrustrado/diaadiahelpers/raw/master/PORTAL_extracao_de_audio
+wget --no-check-certificate -O ~/.gnome2/nautilus-scripts/PORTAL_extracao_de_audio http://github.com/artistafrustrado/diaadiahelpers/raw/master/PORTAL_extracao_de_audio
 chmod a+x ~/.gnome2/nautilus-scripts/PORTAL_extracao_de_audio
 
